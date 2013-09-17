347f3d50c7e04af5b9776d5c810efa87ea566c43
Florian Grundig <florian.grundig@gmx.de>
2013-09-17 14:53:31 +0200

    fixed wrong path to karma-e2e.conf.js

--- test/functional/de/is24/demo/karma/JavaScriptE2eTestKarmaSuite.java
+++ test/functional/de/is24/demo/karma/JavaScriptE2eTestKarmaSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(KarmaTestSuiteRunner.class)
-@KarmaTestSuiteRunner.KarmaConfigPath("./src/test/resources/config/karma-e2e.conf.js")
+@KarmaTestSuiteRunner.KarmaConfigPath("./test/javascript/config/karma-e2e.conf.js")
 //@KarmaTestSuiteRunner.KarmaProcessBuilderArgs("./scripts/e2e-test.sh") // i need this when running on mac to get karma to run
 public class JavaScriptE2eTestKarmaSuite {
 
