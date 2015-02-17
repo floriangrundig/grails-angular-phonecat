af90ca1000a1ac19f3eb3486853c24c15a78387c
flg <flg@trifork.com>
2015-02-17 20:57:28 +0100

    upgrade to grails 2.4.4

--- test/unit/de/is24/demo/karma/JavaScriptUnitTestKarmaSuite.java
+++ test/unit/de/is24/demo/karma/JavaScriptUnitTestKarmaSuite.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(KarmaTestSuiteRunner.class)
 //@KarmaTestSuiteRunner.KarmaProcessName("./test/javascript/config/karma.conf.js")
+@KarmaTestSuiteRunner.KarmaProcessName("./node_modules/karma/bin/karma")
 @KarmaTestSuiteRunner.KarmaConfigPath("./test/javascript/config/karma.conf.js")
 @KarmaTestSuiteRunner.KarmaRemoteServerPort(9877)
 public class JavaScriptUnitTestKarmaSuite {
